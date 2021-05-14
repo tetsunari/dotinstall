13f6c1d70d3ba4d43a6596b4170b758383355081
tsunari <mtst.tty556@gmail.com>
2021-05-14 22:59:08 +0900

    css追加

--- todo_app_php_v4-master/work/public/index.php
+++ todo_app_php_v4-master/work/public/index.php
@@ -24,8 +24,24 @@ try {
 <head>
 	<meta charset="utf-8">
 	<title>My Todos</title>
+	<link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
 <h1>Todos</h1>
+
+<ul>
+	<li>
+		<input type="checkbox">
+		<span>Title</span>
+	</li>
+	<li>
+		<input type="checkbox" checked>
+		<span class="done">Title</span>
+	</li>
+	<li>
+		<input type="checkbox">
+		<span>Title</span>
+	</li>
+</ul>
 </body>
 </html>
