012a6460898496a277ac0840e2409d8a05c5524d
tetsunari <mtst.tty556@gmail.com>
2021-08-21 19:58:30 +0900

    クエリ文字列

--- todo_app_php_v4-master/work/public/index.php
+++ todo_app_php_v4-master/work/public/index.php
@@ -8,7 +8,16 @@ $pdo = getPdoInstance();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	validateToken();
-	addTodo($pdo);
+	$action = filter_input(INPUT_GET, 'action');
+
+	switch ($action) {
+		case 'add':
+			addTodo($pdo);
+			break;
+		case 'toggle':
+			toggleTodo($pdo);
+			break;
+	}
 
 	header('Location: ' . SITE_URL);
 	exit;
@@ -27,7 +36,7 @@ $todos = getTodos($pdo);
 <body>
 <h1>Todos</h1>
 
-<form action="" method="post">
+<form action="?action=add" method="post">
 	<input type="text" name="title" placeholder="Type new todo.">
 	<input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
 </form>
@@ -35,7 +44,12 @@ $todos = getTodos($pdo);
 <ul>
 	<?php foreach ($todos as $todo): ?>
 		<li>
-			<input type="checkbox" <?= $todo->is_done ? 'checked' : ''; ?>>
+			<form action="?action=toggle" method="post">
+				<input type="checkbox" <?= $todo->is_done ? 'checked' : ''; ?>>
+				<input type="hidden" name="id" value="<?= h($todo->id); ?>">
+				<input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
+			</form>
+
 			<span class="<?= $todo->is_done ? 'done' : ''; ?>">
         <?= h($todo->title); ?>
       </span>
