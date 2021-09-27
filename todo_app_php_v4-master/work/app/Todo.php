219644902a0fa009b1197b233e7a4a482419aac3
tetsunari <mtst.tty556@gmail.com>
2021-09-28 08:57:50 +0900

    add todo function

--- todo_app_php_v4-master/work/app/Todo.php
+++ todo_app_php_v4-master/work/app/Todo.php
@@ -1,6 +1,5 @@
 <?php
 
-
 class Todo
 {
 	private $pdo;
@@ -8,11 +7,68 @@ class Todo
 	public function __construct($pdo)
 	{
 		$this->pdo = $pdo;
+		Token::create();
 	}
 
 	public function processPost()
 	{
+		if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+			Token::validate();
+			$action = filter_input(INPUT_GET, 'action');
 
+			switch ($action) {
+				case 'add':
+					$this->add();
+					break;
+				case 'toggle':
+					$this->toggle();
+					break;
+				case 'delete':
+					$this->delete();
+					break;
+				default:
+					exit;
+			}
+
+			header('Location: ' . SITE_URL);
+			exit;
+		}
+	}
+
+	private function add()
+	{
+		$title = trim(filter_input(INPUT_POST, 'title'));
+		if ($title === '') {
+			return;
+		}
+
+		$stmt = $this->pdo->prepare("INSERT INTO todos (title) VALUES (:title)");
+		$stmt->bindValue('title', $title, PDO::PARAM_STR);
+		$stmt->execute();
+	}
+
+	private function toggle()
+	{
+		$id = filter_input(INPUT_POST, 'id');
+		if (empty($id)) {
+			return;
+		}
+
+		$stmt = $this->pdo->prepare("UPDATE todos SET is_done = NOT is_done WHERE id = :id");
+		$stmt->bindValue('id', $id, PDO::PARAM_INT);
+		$stmt->execute();
+	}
+
+	private function delete()
+	{
+		$id = filter_input(INPUT_POST, 'id');
+		if (empty($id)) {
+			return;
+		}
+
+		$stmt = $this->pdo->prepare("DELETE FROM todos WHERE id = :id");
+		$stmt->bindValue('id', $id, PDO::PARAM_INT);
+		$stmt->execute();
 	}
 
 	public function getAll()
--- todo_app_php_v4-master/work/public/index.php
+++ todo_app_php_v4-master/work/public/index.php
@@ -2,37 +2,11 @@
 
 require_once(__DIR__ . '/../app/config.php');
 
-Token::create();
-
 $pdo = Database::getInstance();
 
 $todo = new Todo($pdo);
 $todo->processPost();
 $todos = $todo->getAll();
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	Token::validate();
-	$action = filter_input(INPUT_GET, 'action');
-
-	switch ($action) {
-		case 'add':
-			addTodo($pdo);
-			break;
-		case 'toggle':
-			toggleTodo($pdo);
-			break;
-		case 'delete':
-			deleteTodo($pdo);
-			break;
-		default:
-			exit;
-	}
-
-	header('Location: ' . SITE_URL);
-	exit;
-}
-
-$todos = getTodos($pdo);
 
 ?>
 <!DOCTYPE html>
