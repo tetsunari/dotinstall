b834b72c5e7ed1b337ba1e09a20b3ad5e6c876d0
tetsunari <mtst.tty556@gmail.com>
2021-11-25 12:22:56 +0900

    文字列扱い

---
+++ js_practice_2/js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// console.log('hello');
+// console.log("hello");
+
+// console.log('it's me!');
+// console.log("it's me!");
+// console.log('it\'s me!');
+
+// console.log('hel\nlo wor\tld');
+
+console.log('hello' + 'world');
