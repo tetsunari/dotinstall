7c248d1c35f0fca3253f68ee2dfdaee69980a622
tetsunari <mtst.tty556@gmail.com>
2021-11-30 00:18:29 +0900

    push shift

--- js_practice_3/js/main.js
+++ js_practice_3/js/main.js
@@ -2,10 +2,7 @@
 
 {
     const scores = [80, 90, 40, 70];
-
-    // console.log(`Score: ${scores[0]}`);
-    // console.log(`Score: ${scores[1]}`);
-    // console.log(`Score: ${scores[2]}`);
+    scores.splice(1, 1, 40, 50);
 
     for (let i = 0; i < scores.length; i++) {
         console.log(`Score ${i}: ${scores[i]}`);
