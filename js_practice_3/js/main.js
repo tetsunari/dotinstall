df590f22df7378999be87bb73e3d64ca1e2ac53a
tetsunari <mtst.tty556@gmail.com>
2022-03-23 16:25:47 +0900

    date応用

--- js_practice_3/js/main.js
+++ js_practice_3/js/main.js
@@ -1,8 +1,10 @@
 'use strict';
 
 {
-    const d = new Date();
+    const d = new Date(2019, 10); // 2019/11/01 00:00:00
+    //年と月は必須
+    d.setHours(10, 20, 30); // 2019/11/01 10:20:30
+    d.setDate(31); // 2019/12/01 10:20:30
+    d.setDate(d.getDate() + 3); // 2019/12/04 10:20:30
     console.log(d);
-
-    console.log(`${d.getMonth() + 1} 月 ${d.getDate()} 日`);
 }
