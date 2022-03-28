6822d9bb2e826b8168bbf9da3f6943abec8bc6b5
tetsunari <mtst.tty556@gmail.com>
2022-03-28 09:00:42 +0900

    querySelectorAll

--- js_dom/js/main.js
+++ js_dom/js/main.js
@@ -2,9 +2,12 @@
 
 {
     function update() {
-        // document.querySelector('h1').textContent = 'Changed!';
-        // document.querySelector('#target').textContent = 'Changed!';
-        document.getElementById('target').textContent = 'Changed!';
+        // document.getElementById('target').textContent = 'Changed!';
+        // document.querySelector('p').textContent = 'Changed!';
+        // document.querySelectorAll('p')[1].textContent = 'Changed!';
+        document.querySelectorAll('p').forEach((p, index) => {
+            p.textContent = `${index}番目のpです！`;
+        });
     }
 
     setTimeout(update, 1000);
