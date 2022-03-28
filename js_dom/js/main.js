2989219f00eb7545bcab3eeee7b9280180a1d283
tetsunari <mtst.tty556@gmail.com>
2022-03-29 00:59:44 +0900

    addEventLisner

--- js_dom/js/main.js
+++ js_dom/js/main.js
@@ -1,14 +1,7 @@
 'use strict';
 
 {
-    function update() {
-        // document.getElementById('target').textContent = 'Changed!';
-        // document.querySelector('p').textContent = 'Changed!';
-        // document.querySelectorAll('p')[1].textContent = 'Changed!';
-        document.querySelectorAll('p').forEach((p, index) => {
-            p.textContent = `${index}番目のpです！`;
-        });
-    }
-
-    setTimeout(update, 1000);
+    document.querySelector('button').addEventListener('click', () => {
+        document.getElementById('target').textContent = 'Changed!';
+    });
 }
