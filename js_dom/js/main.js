0cce91802e9e57f25554507ca13aa1cceb28a53f
tetsunari <mtst.tty556@gmail.com>
2022-03-31 16:47:05 +0900

    カスタムデータ属性

--- js_dom/js/main.js
+++ js_dom/js/main.js
@@ -4,14 +4,7 @@
     document.querySelector('button').addEventListener('click', () => {
         const targetNode = document.getElementById('target');
 
-        // targetNode.className = 'my-color my-border';
-        // targetNode.classList.add('my-color');
-
-        // if (targetNode.classList.contains('my-color') === true) {
-        //   targetNode.classList.remove('my-color');
-        // } else {
-        //   targetNode.classList.add('my-color');
-        // }
-        targetNode.classList.toggle('my-color');
+        // targetNode.textContent = 'Dotinstall';
+        targetNode.textContent = targetNode.dataset.translation;
     });
 }
