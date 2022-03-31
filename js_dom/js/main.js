7a5a1de8930e430b7743e8ccff67348fe86bbaf9
tetsunari <mtst.tty556@gmail.com>
2022-03-31 17:02:01 +0900

    cloneNode, insertBefore

--- js_dom/js/main.js
+++ js_dom/js/main.js
@@ -2,11 +2,15 @@
 
 {
     document.querySelector('button').addEventListener('click', () => {
-        const item2 = document.createElement('li');
-        item2.textContent = 'item 2';
+        const item = document.querySelectorAll('li');
+        item.forEach(itemValue => console.log(itemValue.textContent));
+        const item0 = item[0];
+        const copy = item0.cloneNode(true);
+        const copyFalse = item0.cloneNode(false);
+        console.log(copyFalse);
 
-        // const ulNode = document.querySelector('ul');
         const ul = document.querySelector('ul');
-        ul.appendChild(item2);
+        const item2 = document.querySelectorAll('li')[2];
+        ul.insertBefore(copy, item2);
     });
 }
